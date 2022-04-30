ec40c8e185aa783c64c933e8bd47b8b574469b6a
wangdisheng <wangdishengyx@163.com>
2022-04-30 23:31:06 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        // filename: 'main.js',
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
