03b765a84f778287d4ec1e0c6036cab0a2a3ff0c
wangdisheng <wangdishengyx@163.com>
2022-05-01 16:46:24 +0800

    使用 webpack-dev-server 以及提取 css 文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,41 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+
+// "style-loader"
+
 module.exports = {
     mode: "development",
     entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: "./dist"
+    },
     output: {
         // filename: 'main.js',
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
     },
+    optimization: {
+        runtimeChunk: 'single',
+    },
     plugins: [new HtmlWebpackPlugin({
         title: 'dswang',
-        template:  'src/assets/index.html'
+        template: 'src/assets/index.html'
+    }), new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
+        chunkFilename: "[id].[contenthash].css",
     })],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: "/public/path/to/",
+                    },
+                }, "css-loader"],
             },
         ],
     },
